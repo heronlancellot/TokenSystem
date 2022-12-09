6456a1732bf78fb421bb337aa1e2a002e480fb75
heronlancellot <heronlancellot@id.uff.br>
2022-12-09 14:38:10 -0300

    Delete React-app and Initialize nextjs

--- frontend/src/index.js
+++
@@ -1,7 +0,0 @@
-import App from './App'
-import './index.css'
-import { createRoot } from "react-dom/client";
-const container = document.querySelector("#root");
-const root = createRoot(container);
-
-root.render(<App tab="home" />);
